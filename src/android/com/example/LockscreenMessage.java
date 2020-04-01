40115ede70019c458a78162ff5de888809f7bb8b
th <th@wmc-card.no>
2020-04-01 11:41:48 +0200

    notification

--- src/android/com/example/LockscreenMessage.java
+++ src/android/com/example/LockscreenMessage.java
@@ -12,6 +12,10 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.Date;
